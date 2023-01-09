612a8cc61aeaa35d8e0e67a10402e26e8e8999fa
TRONG VUONG <tronghandsome1111@gmail.com>
2023-01-10 05:41:06 +0700

    upload

--- doan/src/main/java/com/code/repository/maxacnhanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.code.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.code.model.maxacnhan;
-
-public interface maxacnhanRepository extends JpaRepository<maxacnhan,Integer> {
-
-}
